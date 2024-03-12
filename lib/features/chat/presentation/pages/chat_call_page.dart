cbcd439ac8bb78604faed256b555265eecc15a86
ardeshir <ardeshirojan2@gmail.com>
2024-03-12 04:01:03 +0330

    notification tab

--- lib/features/chat/presentation/pages/chat_call_page.dart
+++ lib/features/chat/presentation/pages/chat_call_page.dart
@@ -4,6 +4,7 @@ import 'package:messaging_core/core/utils/extensions.dart';
 import 'package:messaging_core/features/chat/presentation/pages/chat_list_page.dart';
 import 'package:messaging_core/features/chat/presentation/widgets/animated_app_bar.dart';
 import 'package:messaging_core/features/chat/presentation/widgets/chat_list_widgets/chat_call_top_layout.dart';
+import 'package:messaging_core/features/chat/presentation/widgets/chat_list_widgets/notifications_tab.dart';
 import 'package:messaging_core/features/chat/presentation/widgets/chat_list_widgets/user_contacts_drawer.dart';
 import 'package:messaging_core/features/chat/presentation/widgets/chat_page_widgets/chat_page_drawer.dart';
 
@@ -72,9 +73,7 @@ class _ChatCallPageState extends State<ChatCallPage>
                     child: TabBarView(
                       controller: tabController,
                       children: [
-                        const Column(
-                          children: [],
-                        ),
+                        NotificationsTab(),
                         const Column(
                           children: [],
                         ),
---
+++ lib/features/chat/presentation/widgets/chat_list_widgets/notifications_tab.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:messaging_core/app/theme/app_text_styles.dart';
+import 'package:messaging_core/app/theme/constants.dart';
+import 'package:messaging_core/app/widgets/icon_widget.dart';
+import 'package:messaging_core/app/widgets/text_widget.dart';
+import 'package:messaging_core/core/utils/extensions.dart';
+import 'package:messaging_core/features/chat/presentation/widgets/animated_app_bar.dart';
+
+class NotificationsTab extends StatelessWidget {
+  const NotificationsTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: context.screenHeight / 1.4,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const IconWidget(
+            icon: Assets.callRemove2,
+            size: 100,
+          ),
+          const SizedBox(height: 10),
+          TextWidget(
+            tr(context).noNotification,
+            style: AppTextStyles.subtitle,
+          ),
+          const SizedBox(height: 10),
+          TextWidget(
+            tr(context).noChatDesc,
+            style: AppTextStyles.body4.copyWith(fontSize: 18),
+          ),
+        ],
+      ),
+    );
+  }
+}
