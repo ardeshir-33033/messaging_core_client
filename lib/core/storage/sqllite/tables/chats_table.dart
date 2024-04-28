e58d6195b44e1d212ccc2bc123cc92b8897730cb
ardeshir <ardeshirojan2@gmail.com>
2024-04-28 04:17:38 +0330

    removed tables

--- lib/core/storage/sqllite/tables/chats_table.dart
+++ lib/core/storage/sqllite/tables/chats_table.dart
@@ -1,32 +1,32 @@
-import 'package:drift/drift.dart';
-
-class ChatsTable extends Table {
-  IntColumn get id => integer()();
-
-  IntColumn get creatorUserId => integer().nullable()();
-
-  IntColumn get categoryId => integer()();
-
-  IntColumn get level => integer().nullable()();
-
-  TextColumn get name => text().nullable()();
-
-  TextColumn get avatar => text().nullable()();
-
-  TextColumn get username => text().nullable()();
-
-  TextColumn get status => text().nullable()();
-
-  TextColumn get groupUsers => text().nullable()();
-
-  TextColumn get lastMessage => text().nullable()();
-
-  TextColumn get lastRead => text().nullable()();
-
-  DateTimeColumn get createdAt => dateTime().nullable()();
-
-  DateTimeColumn get updatedAt => dateTime().nullable()();
-
-  @override
-  Set<Column> get primaryKey => {id};
-}
+// import 'package:drift/drift.dart';
+//
+// class ChatsTable extends Table {
+//   IntColumn get id => integer()();
+//
+//   IntColumn get creatorUserId => integer().nullable()();
+//
+//   IntColumn get categoryId => integer()();
+//
+//   IntColumn get level => integer().nullable()();
+//
+//   TextColumn get name => text().nullable()();
+//
+//   TextColumn get avatar => text().nullable()();
+//
+//   TextColumn get username => text().nullable()();
+//
+//   TextColumn get status => text().nullable()();
+//
+//   TextColumn get groupUsers => text().nullable()();
+//
+//   TextColumn get lastMessage => text().nullable()();
+//
+//   TextColumn get lastRead => text().nullable()();
+//
+//   DateTimeColumn get createdAt => dateTime().nullable()();
+//
+//   DateTimeColumn get updatedAt => dateTime().nullable()();
+//
+//   @override
+//   Set<Column> get primaryKey => {id};
+// }
--- lib/core/storage/sqllite/tables/message_table.dart
+++ lib/core/storage/sqllite/tables/message_table.dart
@@ -1,38 +1,38 @@
-import 'package:drift/drift.dart';
-
-class MessageTable extends Table {
-  IntColumn get id => integer()();
-
-  IntColumn get receiverId => integer()();
-
-  IntColumn get categoryId => integer()();
-
-  IntColumn get senderId => integer()();
-
-  TextColumn get receiverType => text()();
-
-  TextColumn get messageText => text()();
-
-  TextColumn get messageType => text()();
-
-  TextColumn get filePath => text().nullable()();
-
-  TextColumn get sender => text().nullable()();
-
-  IntColumn get pinned => integer()();
-
-  TextColumn get replied => text().nullable()();
-
-  TextColumn get roomIdentifier => text()();
-
-  BoolColumn get isForwarded => boolean().withDefault(const Constant(false))();
-
-  DateTimeColumn get createdAt => dateTime()();
-
-  DateTimeColumn get updatedAt => dateTime()();
-
-  DateTimeColumn get readedAt => dateTime().nullable()();
-
-  @override
-  Set<Column> get primaryKey => {id};
-}
+// import 'package:drift/drift.dart';
+//
+// class MessageTable extends Table {
+//   IntColumn get id => integer()();
+//
+//   IntColumn get receiverId => integer()();
+//
+//   IntColumn get categoryId => integer()();
+//
+//   IntColumn get senderId => integer()();
+//
+//   TextColumn get receiverType => text()();
+//
+//   TextColumn get messageText => text()();
+//
+//   TextColumn get messageType => text()();
+//
+//   TextColumn get filePath => text().nullable()();
+//
+//   TextColumn get sender => text().nullable()();
+//
+//   IntColumn get pinned => integer()();
+//
+//   TextColumn get replied => text().nullable()();
+//
+//   TextColumn get roomIdentifier => text()();
+//
+//   BoolColumn get isForwarded => boolean().withDefault(const Constant(false))();
+//
+//   DateTimeColumn get createdAt => dateTime()();
+//
+//   DateTimeColumn get updatedAt => dateTime()();
+//
+//   DateTimeColumn get readedAt => dateTime().nullable()();
+//
+//   @override
+//   Set<Column> get primaryKey => {id};
+// }
